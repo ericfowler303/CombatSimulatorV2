71f40fd77c100fbd84940c0a33e7670c0c55c612
Eric Fowler <ericfowler303@gmail.com>
2014-11-16 15:57:16 -0700

    Implement most of the Player attacks

--- CombatSimulatorV2/CombatSimulatorV2/Program.cs
+++ CombatSimulatorV2/CombatSimulatorV2/Program.cs
@@ -22,6 +22,7 @@ namespace CombatSimulatorV2
             SmashPrinter,
             AccountingVirus,
         }
+        private Random rng = new Random();
         private bool HasPrinterBeenSmashed { get; set; }
         private bool AccountingVirusRunning { get; set; }
         private double VirusMoney { get; set; }
@@ -38,7 +39,58 @@ namespace CombatSimulatorV2
         }
         public void DoAttack(Enemy enemy)
         {
+            double tempPlayerDamage = 0.0;
+            switch (ChooseAttack())
+            {
+                case AttackType.Tetris:
+                    if (rng.NextDouble() >= 0.3) { tempPlayerDamage = rng.Next(20, 36); enemy.HP -= tempPlayerDamage; }
+                    if (tempPlayerDamage > 0.0) { Console.WriteLine("Playing Tetris hit {0} for ${1} of wasted company money", enemy.Name, tempPlayerDamage); }
+                    else
+                    {
+                        Console.WriteLine("Somehow playing Tetris didn't waste any of {0}'s money today.", enemy.Name);
+                    }
+                    break;
+                case AttackType.BurnTPS:
+                    tempPlayerDamage = rng.Next(10, 16);
+                    Console.WriteLine("Burning TPS Reports hit {0} for ${1} of wasted company money", enemy.Name, tempPlayerDamage);
+                    break;
+                case AttackType.Heal:
+                    this.HP += rng.Next(10, 21);
+                    Console.WriteLine("Spending time with Joanna has helped you heal your wounds from your miserable cubicle life.");
+                    break;
+                case AttackType.SmashPrinter:
+                    if (this.HasPrinterBeenSmashed) { if (rng.NextDouble() >= 0.5) { tempPlayerDamage = rng.Next(20, 36) * (rng.NextDouble() + 1); } }
+                    else
+                    {
+                        // The attack missed
+                        Console.WriteLine("That damn printer failed you on a daily basis and how it's busted up part failed you as well.");
+                    }
+                    if (!this.HasPrinterBeenSmashed) 
+                    {
+                        // On first attack, smash the printer so the broken part can be used the next time
+                        Console.WriteLine("You really showed that jammed up printer who's boss."); this.HasPrinterBeenSmashed = true;
+                    }
+                    else
+                    {
+                        // The attack hit, print the result
+                        Console.WriteLine("That busted printer part hit {0} for ${1} of wasted company money", enemy.Name, tempPlayerDamage);
+                    }
+                    break;
+                case AttackType.AccountingVirus:
+                    if (!this.AccountingVirusRunning)
+                    {
+                        // Start the virus
+                        this.AccountingVirusRunning = true;
+                    } // Print this line if has been running already or it just started running
+                    Console.WriteLine("The accounting virus is on the loose, hopefully it only steals a few fractions of a penny at a time.");
+                    break;
+                default:
+                    Console.WriteLine("Invalid attack choice");
+                    break;
+            }
 
+            // Check if the virus is running and have it run every turn
+            if (this.AccountingVirusRunning) { VirusRunner(); }
         }
         private AttackType ChooseAttack()
         {
@@ -78,6 +130,15 @@ namespace CombatSimulatorV2
                     // Return 0 if the input was invalid
                 default: return 0;
             }
+        }
+        private void VirusRunner()
+        {
+            // Steal money from the company, a fraction of a penny at a time, which happens to be surprisingly more then expected
+            double virusTemp = this.VirusMoney * (rng.NextDouble() + 2.1);
+            this.VirusMoney = virusTemp;
+            // Give the play the extra stolen money
+            this.HP += virusTemp;
+
         }
     }
 
