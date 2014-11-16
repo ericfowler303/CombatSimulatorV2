ddcc43630b90c628025a527710c48b0b3c725747
Eric Fowler <ericfowler303@gmail.com>
2014-11-16 14:33:40 -0700

    Implement Enemy Class
    still needs some console messages

---
+++ CombatSimulatorV2/CombatSimulatorV2/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CombatSimulatorV2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+
+    class Enemy
+    {
+        private Random rng = new Random();
+        public string Name { get; set; }
+        public double HP { get; set; }
+        public bool IsAlive
+        {
+            get { if (this.HP > 0.0) { return true; } else { return false; } }
+        }
+        
+        public Enemy(string name, double health)
+        {
+            this.Name = name;
+            this.HP = health;
+        }
+
+        public void DoAttack(Player player)
+        {
+            // Has a 80% chance of hitting
+            if (rng.NextDouble() >= 0.2)
+            {
+                // A hit, between $5-$15 loss for the player
+                player.HP -= rng.Next(5, 16);
+            }
+        }
+    }
+}
